6ee72c07a0a6e258269b5a2d466e20d95d4cb751
Kody <kodyckendall@gmail.com>
2024-09-07 17:44:43 -0700

    Fix: llama scaffold generator

---
+++ test/lib/generators/llama_scaffold_generator_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+require "generators/llama_scaffold/llama_scaffold_generator"
+
+class LlamaScaffoldGeneratorTest < Rails::Generators::TestCase
+  tests LlamaScaffoldGenerator
+  destination Rails.root.join("tmp/generators")
+  setup :prepare_destination
+
+  # test "generator runs without errors" do
+  #   assert_nothing_raised do
+  #     run_generator ["arguments"]
+  #   end
+  # end
+end
