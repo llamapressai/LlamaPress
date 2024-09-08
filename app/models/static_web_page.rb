b7d4e4b38511e64181fb9f1587b50bd31fd9756e
Kody <kodyckendall@gmail.com>
2024-09-08 09:50:13 -0700

    user experience: don't make the user choose a static_web_site. default to grouping sites under the first static_web_site created, or create a new one if this is the first.

--- app/models/static_web_page.rb
+++ app/models/static_web_page.rb
@@ -1,3 +1,32 @@
 class StaticWebPage < ApplicationRecord
-  belongs_to :static_web_site
+  belongs_to :static_web_site, optional: true
+  belongs_to :organization
+
+  before_create :ensure_static_web_site
+
+  private
+
+  def ensure_static_web_site
+    return if static_web_site.present?
+
+    self.static_web_site = organization.static_web_sites.first || create_new_static_web_site
+  end
+
+  def create_new_static_web_site
+    title = generate_unique_title
+    StaticWebSite.create!(title: title, organization: organization)
+  end
+
+  def generate_unique_title
+    base_title = business_name.presence || 'Default Site'
+    title = base_title
+    index = 1
+
+    while StaticWebSite.exists?(title: title)
+      title = "#{base_title} #{index}"
+      index += 1
+    end
+
+    title
+  end
 end
