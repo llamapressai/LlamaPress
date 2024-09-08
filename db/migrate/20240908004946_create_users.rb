66782fb8d2d97d6666037e63d762632967342cde
Kody <kodyckendall@gmail.com>
2024-09-07 17:59:28 -0700

    fix: change users table to not have email, since devise needs to add that field in when initiating

--- db/migrate/20240908004946_create_users.rb
+++ db/migrate/20240908004946_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
-      t.string :email
       t.string :phone
       t.string :first_name
       t.string :last_name
@@ -9,7 +8,6 @@ class CreateUsers < ActiveRecord::Migration[7.2]
 
       t.timestamps
     end
-    add_index :users, :email, unique: true
     add_index :users, :phone
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,49 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.2].define(version: 2024_09_08_005144) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "phone"
+    t.text "notes"
+    t.string "state"
+    t.string "street_address"
+    t.string "zip_code"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "phone"
+    t.string "first_name"
+    t.string "last_name"
+    t.bigint "organization_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["organization_id"], name: "index_users_on_organization_id"
+    t.index ["phone"], name: "index_users_on_phone"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "users", "organizations"
+end
