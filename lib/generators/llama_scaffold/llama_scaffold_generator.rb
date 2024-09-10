59035c71c4f5fe8f62a31981b6e1f983ddd07d9a
Kody <kodyckendall@gmail.com>
2024-09-10 14:21:28 -0700

    fix llama scaffold

--- lib/generators/llama_scaffold/llama_scaffold_generator.rb
+++ lib/generators/llama_scaffold/llama_scaffold_generator.rb
@@ -2,54 +2,56 @@
 require 'rails/generators'
 require 'rails/generators/rails/scaffold/scaffold_generator'
 
-module LlamaScaffold
-  class LlamaScaffoldGenerator < Rails::Generators::ScaffoldGenerator
-    source_root Rails::Generators::ScaffoldGenerator.source_root
+module Generators
+  module LlamaScaffold
+    class LlamaScaffoldGenerator < Rails::Generators::ScaffoldGenerator
+      source_root Rails::Generators::ScaffoldGenerator.source_root
 
-    def add_data_llama_tags
-      append_data_tags_to_views
-    end
-
-    def set_default_layout
-      #TODO: Do we want to have a different layout for this llama_scaffold, than for the entire application? 
-      # Discuss with Danish.
-
-      # The below code will inject the layout into the controller of the new model.
-
-      # inject_into_class(
-      #   File.join("app/controllers", class_path, "#{controller_file_name}_controller.rb"),
-      #   "#{controller_class_name}Controller",
-      #   "  layout 'static_website'\n"
-      # )
-    end
-
-    private
-
-    def append_data_tags_to_views
-      view_files.each do |view_file|
-        insert_data_tags_into_file(view_file)
+      def add_data_llama_tags
+        append_data_tags_to_views
       end
-    end
 
-    def view_files
-      Dir.glob(File.join(destination_root, "app/views/#{controller_file_path}/**/*.html.erb"))
-    end
+      def set_default_layout
+        #TODO: Do we want to have a different layout for this llama_scaffold, than for the entire application? 
+        # Discuss with Danish.
 
-    def insert_data_tags_into_file(view_file)
-      relative_path = view_file.sub("#{destination_root}/", '')
-      content = File.read(view_file)
-      
-      modified_content = content.gsub(/(<[^!%\/][^>]*?)(\s*\/?>)/) do |match|
-        opening_tag = $1
-        closing = $2
-        if opening_tag.include?('data-llama')
-          match
-        else
-          "#{opening_tag} data-llama=\"#{relative_path}\"#{closing}"
+        # The below code will inject the layout into the controller of the new model.
+
+        # inject_into_class(
+        #   File.join("app/controllers", class_path, "#{controller_file_name}_controller.rb"),
+        #   "#{controller_class_name}Controller",
+        #   "  layout 'static_website'\n"
+        # )
+      end
+
+      private
+
+      def append_data_tags_to_views
+        view_files.each do |view_file|
+          insert_data_tags_into_file(view_file)
         end
       end
 
-      File.write(view_file, modified_content)
+      def view_files
+        Dir.glob(File.join(destination_root, "app/views/#{controller_file_path}/**/*.html.erb"))
+      end
+
+      def insert_data_tags_into_file(view_file)
+        relative_path = view_file.sub("#{destination_root}/", '')
+        content = File.read(view_file)
+        
+        modified_content = content.gsub(/(<[^!%\/][^>]*?)(\s*\/?>)/) do |match|
+          opening_tag = $1
+          closing = $2
+          if opening_tag.include?('data-llama')
+            match
+          else
+            "#{opening_tag} data-llama=\"#{relative_path}\"#{closing}"
+          end
+        end
+
+        File.write(view_file, modified_content)
+      end
     end
   end
 end
