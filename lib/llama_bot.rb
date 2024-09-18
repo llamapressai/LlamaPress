f782835b2c6a01c32e6dfdeb082a619f897d25ef
Kody <kodyckendall@gmail.com>
2024-09-18 13:42:14 -0700

    feat: llama_bot can improve the design of an HTML ERB file, and run scaffold commands

--- lib/llama_bot.rb
+++ lib/llama_bot.rb
@@ -4,23 +4,64 @@ require 'json'
 
 module LlamaBot
     class << self
-        def completion(user_message, context=nil, selectedElement=nil, webPageId=nil)
-            response = llama_bot_completion(user_message, context, selectedElement, webPageId)
-            
+        def completion(user_message, context=nil, selected_element=nil, web_page_id=nil)
+            file_contents = fetch_file_contents(context, web_page_id)
+            response = llama_bot_completion(user_message, context, file_contents, selected_element, web_page_id)
+            message_to_user = response['message']
+            handle_decision!(response)
+            return message_to_user
+        end
+
+        def handle_decision!(response)
+            Rails.logger.info("RESPONSE: #{response}")
+            Rails.logger.info("DECISION: #{response['decision']}")
             case response['decision']
+            when "IMPROVE_DESIGN"
+                handle_improve_design(response)
             when "GENERATE_SCAFFOLD_COMMAND"
-                return "Hey! You're in a sandbox mode and are unable to create new database tables or make changes to the source code. If you want a full-fledged llamapress instance, please reach out to our support team. kody@fieldrocket.us."
+                handle_scaffold_command(response)
             end
+        end
+
+        def handle_improve_design(response)
+            improved_html = response['message']
+            web_page = StaticWebPage.find_by(id: response['query_params']['web_page_id'])
+            web_page.update(content: improved_html)
+            return "Got it! Here is the improved design you asked for: #{improved_html}. Refresh your browser to see the new design."
+        end        
+
+        def handle_scaffold_command(response)
+            scaffold_command = response['message']
+            Rails.logger.info("SCAFFOLD COMMAND!: #{scaffold_command}")
+      
+            # Execute the scaffold command
+            scaffold_command = scaffold_command.gsub("scaffold", "llama_scaffold")
+            output = ""
+            IO.popen(scaffold_command) do |io|
+              output = io.read
+            end
+            
+            Rails.logger.info("SCAFFOLD OUTPUT!: #{output}")
+            
+            Rails.logger.info("Now, need to run rails db:migrate! #{output}")
+            
+            output = ""
+            IO.popen("rails db:migrate") do |io|
+              output = io.read
+            end
+      
+            return "Got it! Here is the scaffold command you need to run in your terminal: #{scaffold_command}. I ran the scaffold command for you. Here is the output from the terminal: #{output}. Refresh your browser to see the new page on the left navbar."
         end
 
         private
 
-        def llama_bot_completion(user_message, context=nil, selectedElement=nil, webPageId=nil)
+        def llama_bot_completion(user_message, context=nil, file_contents=nil, selected_element=nil, web_page_id=nil)
             params = {
                 'user_message' => user_message,
                 'context' => context,
-                'selectedElement' => selectedElement,
-                'webPageId' => webPageId
+                'selected_element' => selected_element,
+                'web_page_id' => web_page_id,
+                'file_contents' => file_contents
             }.compact
             
             response = JSON.parse(make_get_request(ENV['LLAMA_BOT_URI'], params))
@@ -31,9 +72,21 @@ module LlamaBot
         def make_get_request(base_url, params)
             uri = URI.parse(base_url)
             uri.query = URI.encode_www_form(params)
-            
             response = Net::HTTP.get_response(uri)
             response.body
+        end
+
+        # Fetch the relevent file contents from the database or the file system
+        def fetch_file_contents(context, web_page_id=nil)
+            file_path = context || "app/views/llama_bot/home.html.erb"
+
+            should_we_edit_html_in_webpage_database = file_path.include?("web_pages/show") && !web_page_id&.empty?
+            @web_page = web_page_id&.empty? ? nil : StaticWebPage.find_by(id: web_page_id)
+            
+            # Load the HTML content from the database or the file system
+            file_contents = should_we_edit_html_in_webpage_database ? @web_page&.content : File.read(file_path)
+
+            return file_contents
         end
     end
 end
