1c3fc009e0a1635504d6f92fa6d8caa1a11b3db4
Kody <kodyckendall@gmail.com>
2024-10-05 07:34:50 -0700

    ux fix: mobile experience -- allow all browsers, and make application layout mobile responsive

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
   before_action :authenticate_user!, :set_context
 
   def current_organization
