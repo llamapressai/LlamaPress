a52aad862d95f56ddb69a2de658db6b4e215f2ed
Kody <kodyckendall@gmail.com>
2024-10-18 11:35:53 -0700

    notify kody when new user signs up in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
 class User < ApplicationRecord
+  require 'twilio'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :organization
   accepts_nested_attributes_for :organization
+  after_create :notify_registration
 
   belongs_to :default_site, class_name: "Site", optional: true
 
   # If you want to allow the user to optionally create an org, add:
   before_validation :create_default_organization, on: :create
+
+  def notify_registration
+    Twilio.send_text("9152845787", "New User Registration: email: #{self.email}, organization: #{self.organization.name}, name: #{self.first_name} #{self.last_name}, phone: #{self.phone}")
+  end
 
   private
 
