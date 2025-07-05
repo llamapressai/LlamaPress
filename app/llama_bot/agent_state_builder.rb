218d149b223f6daaec28a10a6412a73f5919bd5f
Kody Kendall <kodyckendall@gmail.com>
2025-07-05 08:46:23 -0700

    feat: write html back to webpage

--- app/llama_bot/agent_state_builder.rb
+++ app/llama_bot/agent_state_builder.rb
@@ -9,13 +9,14 @@ module LlamaPress
       @context = context
     end
 
-    def build
+    def build #Whitelist parameters that were sent in by the user.
       {
-        message: @params[:message], # Rails param from JS/chat UI. This is the user's message to the agent.
-        thread_id: @context[:thread_id], # This is the thread id for the agent. It is used to track the conversation history.
+        message: @params["message"], # Rails param from JS/chat UI. This is the user's message to the agent.
+        thread_id: @params["thread_id"], # This is the thread id for the agent. It is used to track the conversation history.
         api_token: @context[:api_token], # This is an authenticated API token for the agent, so that it can authenticate with us. (It may need access to resources on our Rails app, such as the Rails Console.)
         agent_prompt: LlamaBotRails.agent_prompt_text, # System prompt instructions for the agent. Can be customized in app/llama_bot/prompts/agent_prompt.txt
-        agent_name: "llamapress" # This routes to the appropriate LangGraph agent as defined in LlamaBot/langgraph.json, and enables us to access different agents on our LlamaBot server.
+        agent_name: "llamapress", # This routes to the appropriate LangGraph agent as defined in LlamaBot/langgraph.json, and enables us to access different agents on our LlamaBot server.
+        page_id: @params["page_id"]
       }
     end
   end
